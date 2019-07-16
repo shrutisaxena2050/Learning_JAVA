3f6804723c8e700fe2466f387c673029d8eb0770
Shruti Saxena <40051766+shrutisaxena2050@users.noreply.github.com>
2019-07-16 11:27:02 +0530

    Update Datatypeinput.java

--- Datatypeinput.java
+++ Datatypeinput.java
@@ -1,25 +1,34 @@
 import java.util.*;
 public class Datatypeinput{
     public static void main(String []args){
-    Scanner sc = new Scanner(System.in);
-    byte bytenum = sc.nextByte();
-    System.out.println("Your byte value is: " +bytenum);
-    short shortnum = sc.nextShort();
-    System.out.println("Your short value is: " +shortnum);
-    char word = sc.next().charAt(0);
-    System.out.println("Your char value is: " +word);
-    int intnum = sc.nextInt();
-    System.out.println("Your integer value is: " +intnum);
-    float floatnum = sc.nextFloat();
-    System.out.println("Your float value is: " +floatnum);
-    long longnum = sc.nextLong();
-    System.out.println("Your long value is: " +longnum);
-    double doublenum = sc.nextDouble();
-    System.out.println("Your double value is: " +doublenum);
-    String name = sc.nextLine();
-    System.out.println("Your string value is: " +name);
-    boolean b = sc.nextBoolean();
-    System.out.println("Your boolean value is: " +b);
+    Scanner sc = new Scanner(System.in);                      // Creating Object of Scanner Class
+        
+    byte bytenum = sc.nextByte();                            // Taking input of byte type from user
+    System.out.println("Your byte value is: " +bytenum);     // Displaying the input
+        
+    short shortnum = sc.nextShort();                        //Taking input of short type from user
+    System.out.println("Your short value is: " +shortnum);  // Displaying the input
+        
+    char word = sc.next().charAt(0);                        // Taking input of char type from user
+    System.out.println("Your char value is: " +word);       // Displaying the input
+        
+    int intnum = sc.nextInt();                              // Taking input of int type from user
+    System.out.println("Your integer value is: " +intnum);  // Displaying the input
+        
+    float floatnum = sc.nextFloat();                        // Taking input of float type from user
+    System.out.println("Your float value is: " +floatnum);  // Displaying the input
+        
+    long longnum = sc.nextLong();                           // Taking input of long type from user
+    System.out.println("Your long value is: " +longnum);    // Displaying the input
+        
+    double doublenum = sc.nextDouble();                     // Taking input of double type from user
+    System.out.println("Your double value is: " +doublenum); // Displaying the input
+        
+    String name = sc.nextLine();                            // Taking input of string type from user
+    System.out.println("Your string value is: " +name);     // Displaying the input
+        
+    boolean b = sc.nextBoolean();                           // Taking input of boolean type from user
+    System.out.println("Your boolean value is: " +b);       // Displaying the input
     
 
     }
