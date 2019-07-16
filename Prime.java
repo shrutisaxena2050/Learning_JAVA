72bd8b80ad6280054add61d170848f50a0476051
Shruti Saxena <40051766+shrutisaxena2050@users.noreply.github.com>
2019-07-17 00:00:35 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,27 +1,30 @@
 import java.util.*;
 
-
 public class Prime{
 public static void main(String[]args){
+    
     Scanner scan= new Scanner(System.in);
     int num= scan.nextInt();
     System.out.println("number:" +num);
+    boolean ifprime=true;
 
-    if(num==0|| num==1){
-        System.out.println("the num is not prime");
+    if(num==0|| num==1){                                   //Condition of not prime
+        System.out.println("the num is not prime");                   
     }
     else{
-    for(int i=2; i<=num/2; i++){
+    for(int i=2; i<=num/2; i++){                         //Condition of not prime
         if(num%i==0){
-            System.out.println("the num is not prime");
-            break;
-        }
-        else{
-            System.out.println("the num is prime");
+            ifprime=false;
             break;
         }
           }
     
+    }
+    if(ifprime){                                        //Outputs of conditions
+        System.out.println("the num is prime");        
+    }
+    else{
+        System.out.println("the num is not prime");
     }
 }
 }
