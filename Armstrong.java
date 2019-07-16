c5732f6a29928bd1285ca0a1187adeb63967acb4
Shruti Saxena <40051766+shrutisaxena2050@users.noreply.github.com>
2019-07-16 22:55:47 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -5,29 +5,33 @@ import java.util.*;
 
 public class Armstrong{
 public static void main(String[]args){
-    Scanner scan= new Scanner(System.in);
+    
+    Scanner scan= new Scanner(System.in);            //Create object
     int num= scan.nextInt();
     System.out.println("number:" +num);
     int a = num;
     int n=0;
-    while(a > 0){                 //To find the length of the num
+    
+    while(a > 0){                                 //To find the length of the num
         a= a/10;
         n=n+1;
      }
+    
     int b = num;
     int rem;
     int sum=0;
     
-    while(b>0){
+    while(b>0){                                //Multiplication of digit
         int mul=1;
         rem=b%10;
         for(int i=1; i<=n; i++){
             mul=mul*rem;
 
         }
-        sum=sum+mul;
+        sum=sum+mul;                          //Addition of multiplied number
         b=b/10;
     }
+    
     if(num==sum){
         System.out.println("The num is Armstrong");
     }
