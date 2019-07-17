67f602aafd5ed4e4e20a52cd14e9c4fec9e64506
Shruti Saxena <40051766+shrutisaxena2050@users.noreply.github.com>
2019-07-17 15:24:57 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -3,10 +3,11 @@ public class Factorial{
     public static void main(String []args){
         int num;
         Scanner sc= new Scanner(System.in);
-        num = sc.nextInt();
+        num = sc.nextInt();                 //Taking input from user
         System.out.println("The num is:" +num);
-        long sum=1;
-        for(int i=1;i<=num;i++){
+        long sum=1;                         //Long is used for large values
+        
+        for(int i=1;i<=num;i++){             //Loop for factorial
             sum=sum*i;
         }
         System.out.println("The factorial of the num is:" +sum);
