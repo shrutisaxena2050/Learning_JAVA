8706fcd68572aa6f8d09b78e1adb27ebec3e397f
Shruti Saxena <40051766+shrutisaxena2050@users.noreply.github.com>
2019-07-17 13:27:33 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,11 +1,13 @@
-import java.util.*;
+import java.util.*;             
 public class Fibonacci{
     public static void main(String []args){
-        int a=-1,b=1,result, num;
+        
+        int a=-1,b=1,result, num;                // Declaration of variables
         Scanner sc= new Scanner(System.in);
-        num = sc.nextInt();
+        num = sc.nextInt();                      //User input of num
         System.out.println("Fibonacci series upto: " +num );
-        for(int i=0;i<num;i++){
+        
+        for(int i=0;i<num;i++){                 //Loop of fibonacci
             result= a+b;
             a=b;
             b=result;
