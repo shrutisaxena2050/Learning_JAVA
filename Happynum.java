b3e24a294ec4e728dc33b2c3e72099e280d60484
Shruti Saxena <40051766+shrutisaxena2050@users.noreply.github.com>
2019-07-19 17:20:46 +0530

    Update Happynum.java

--- Happynum.java
+++ Happynum.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.HashSet;
 public class Happynum{
     public static void main(String []args){
         int num;
@@ -6,30 +7,34 @@ public class Happynum{
         int sum=0;
         Scanner sc=new Scanner(System.in);
         num= sc.nextInt();
+        int temp = num;
+        HashSet<Integer> hSet = new HashSet<Integer>();             //Creating object          
         System.out.println("The num you have entered:" +num);
-        
-        while(num != 0){
-            
-                rem=num%10; 
+
+        while(num != 0){                                          
+ 
+                rem=num%10;                                       //Finding remainder
                 num=num/10;
-                sum = sum + rem*rem;
-                System.out.println(sum);
-                if(sum != 1){ 
-                    num=sum;
-                }
-                else{
-                    System.out.println(sum);
+                sum = sum + rem*rem;                              //Adding the squares of remainder
+                
+                if(num <=0 ){
+                    if(hSet.contains(sum)){                      //Checking if the sum is already in the set
+                        System.out.println("The No is UnHappy");
+                        break;
+                    }
+                    else{
+                        hSet.add(sum);                         //Adding the sum in the set
+                    }    
+
+                    if(sum == 1){
+                        System.out.println("the num is happy");
+                        break;
+                    }
+                    else if(sum != 1){
+                        num = sum;
+                        sum = 0;
+                    }
                 }
         }
-               int temp =num;
-                if(temp==1){
-                System.out.println("the num is happy");
-                }
-                else{
-                    System.out.println("the num is NOT happy");
-                }
-                
-
-
-}
+    }
 }
