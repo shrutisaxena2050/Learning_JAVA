5f30fe871db2fefa0f908f501d22885b0a419026
Shruti Saxena <40051766+shrutisaxena2050@users.noreply.github.com>
2019-07-19 17:02:08 +0530

    Update Perfectnum.java

--- Perfectnum.java
+++ Perfectnum.java
@@ -6,17 +6,19 @@ public class Perfectnum{
             num = sc.nextInt();
             System.out.println("entered num:" +num);
             int sum=0;
-            for(int i=1;i<num; i++){
+        
+            for(int i=1;i<num; i++){                  //Finding and checking divisors
                 if(num%i==0){
-                    sum=sum+i;
-                }
-                }
-                if(sum==num){
+                    sum=sum+i;                        // adding divisors
+                  }
+               }
+        
+                if(sum==num){                        //Checking condition
                     System.out.println("the number is perfect number");
-                }
+                  }
                 else{
                     System.out.println("the number is not a perfect number");
-                }
+                  }
 
-            }
+      }
 }
